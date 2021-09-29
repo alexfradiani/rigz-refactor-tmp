d54c037e50e37e42a1d6b02e991af34ec4e0a567
nelegatti <nelegatti.93@gmail.com>
2021-09-29 13:57:02 -0300

    remove test route injection

--- functions/src/controllers/index.ts
+++ functions/src/controllers/index.ts
@@ -1,12 +1,8 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { CarrierController } from './carrier.controller';
 
 
 export const routes = (router: Router) => {
-  router.get('/test', (req: Request, res: Response) => {
-    res.json(`complete ${req.path}`);
-  });
-
   const carrierCtrl = new CarrierController();
   router.use('/carriers', carrierCtrl.routes(router));
   
