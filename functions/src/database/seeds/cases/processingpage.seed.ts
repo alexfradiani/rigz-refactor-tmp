c11a964a911c83aa6c617616835a95f23d2e1d66
Alex Fradiani <a.fradiani@gmail.com>
2021-10-25 19:52:19 +0200

    refactor processingPage seed

--- functions/src/database/seeds/cases/processingpage.seed.ts
+++ functions/src/database/seeds/cases/processingpage.seed.ts
@@ -8,28 +8,27 @@ import UserSeed from "../entities/user.seed";
 
 export default class ProcessingPageSeed {
   db: Connection;
+  carriersCount = 20;
+  loadsCount = 5;
+  ftCount = 5;
 
   constructor(db: Connection) {
     this.db = db;
   }
 
   async one(): Promise<void> {
-    const carriersCount = 20;
-    const loadsCount = 5;
-    const ftCount = 5;
-
     const carrierSeed = new CarrierSeed(this.db);
-    const carriers = await carrierSeed.many(carriersCount);
+    const carriers = await carrierSeed.many(this.carriersCount);
     console.log("carriers created");
 
     const userSeed = new UserSeed(this.db);
-    const users = await userSeed.many(carriersCount);
+    const users = await userSeed.many(this.carriersCount);
     console.log("users created");
 
     const loadSeed = new LoadSeed(this.db);
     let loads: Load[] = [];
-    for (let i = 0; i < carriersCount; i++) {
-      const newLoads = await loadSeed.withCarrier(carriers[i], loadsCount);
+    for (let i = 0; i < this.carriersCount; i++) {
+      const newLoads = await loadSeed.withCarrier(carriers[i], this.loadsCount);
       users[i].processingLoad = newLoads[0]; // carrier-processing user
       await this.db.manager.save(users[i]);
 
@@ -39,12 +38,12 @@ export default class ProcessingPageSeed {
 
     const ftSeed = new FinancialTransactionSeed(this.db);
     for (let i = 0; i < loads.length; i++) {
-      await ftSeed.withLoad(loads[i], ftCount);
+      await ftSeed.withLoad(loads[i], this.ftCount);
     }
     console.log("financial transactions created");
 
     const cbSeed = new CollectionBoardSeed(this.db);
-    for (let i = 0; i < carriersCount; i++) {
+    for (let i = 0; i < this.carriersCount; i++) {
       await cbSeed.withCarrier(carriers[i]);
     }
     console.log("collection boards created");
